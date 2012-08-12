651c28d84c5c9397d7a5e3361f17e10c0831b126
Axel Pardemann <axelitus2005@gmail.com>
2012-08-11 22:24:27 -0500

    Refactored Num class tests

--- resources/tests/NumTest.php
+++ resources/tests/NumTest.php
@@ -14,11 +14,25 @@ class NumTest extends \PHPUnit_Framework_TestCase
      */
     public function testIsInt()
     {
-        $this->assertEquals(true, Num::isInt(5), "5 as int type");
-        $this->assertEquals(true, Num::isInt("9"), "9 as string type");
-        $this->assertEquals(false, Num::isInt(3.28), "3.28 as float type");
-        $this->assertEquals(false, Num::isInt("this is a string"), "A string without numbers");
-        $this->assertEquals(false, Num::isInt("1. Although this contains a number is not an int"), "A string with a number");
+        $output = Num::isInt(5);
+        $message = "Testing 5 as int type";
+        $this->assertTrue($output, $message);
+
+        $output = Num::isInt("9");
+        $message = "Testing 9 as string type";
+        $this->assertTrue($output, $message);
+
+        $output = Num::isInt(3.28);
+        $message = "Testing 3.28 as float type";
+        $this->assertFalse($output, $message);
+
+        $output = Num::isInt("this is a string");
+        $message = "Testing a string without numbers";
+        $this->assertFalse($output, $message);
+
+        $output = Num::isInt("1. Although this contains a number is not an int");
+        $message = "Testing a string with a number";
+        $this->assertFalse($output, $message);
     }
 
     /**
@@ -26,38 +40,119 @@ class NumTest extends \PHPUnit_Framework_TestCase
      */
     public function testBetween()
     {
+        /**
+         * Tests for value = 5
+         */
         $num_low = 3;
         $num_high = 12;
-        $str = "7";
-
         $value = 5;
-        $this->assertEquals(true, Num::between($value, $num_low, $num_high, false, false), "Is $value between ]{$num_low},{$num_high}[");
-        $this->assertEquals(true, Num::between($value, $num_low, $num_high, false, true), "Is $value between ]{$num_low},{$num_high}]");
-        $this->assertEquals(true, Num::between($value, $num_low, $num_high, true, false), "Is $value between [{$num_low},{$num_high}[");
-        $this->assertEquals(true, Num::between($value, $num_low, $num_high, true, true), "Is $value between [{$num_low},{$num_high}]");
+        
+        $output = Num::between($value, $num_low, $num_high, false, false);
+        $message = "Is $value between ]{$num_low},{$num_high}[";
+        $this->assertTrue($output, $message);
 
+        $output = Num::between($value, $num_low, $num_high, false, true);
+        $message = "Is $value between ]{$num_low},{$num_high}]";
+        $this->assertTrue($output, $message);
+        
+        $output = Num::between($value, $num_low, $num_high, true, false);
+        $message = "Is $value between [{$num_low},{$num_high}[";
+        $this->assertTrue($output, $message);
+        
+        $output = Num::between($value, $num_low, $num_high, true, true);
+        $message = "Is $value between [{$num_low},{$num_high}]";
+        $this->assertTrue($output, $message);
+
+        /**
+         * Tests for value = 7
+         */
+        $num_low = "7";
+        $num_high = 12;
         $value = 7;
-        $this->assertEquals(false, Num::between($value, $str, $num_high, false, false), "Is $value between ]{$str},{$num_high}[");
-        $this->assertEquals(false, Num::between($value, $str, $num_high, false, true), "Is $value between ]{$str},{$num_high}]");
-        $this->assertEquals(true, Num::between($value, $str, $num_high, true, false), "Is $value between [{$str},{$num_high}[");
-        $this->assertEquals(true, Num::between($value, $str, $num_high, true, true), "Is $value between [{$str},{$num_high}]");
 
+        $output = Num::between($value, $num_low, $num_high, false, false);
+        $message = "Is $value between ]{$num_low},{$num_high}[";
+        $this->assertFalse($output, $message);
+
+        $output = Num::between($value, $num_low, $num_high, false, true);
+        $message = "Is $value between ]{$num_low},{$num_high}]";
+        $this->assertFalse($output, $message);
+        
+        $output = Num::between($value, $num_low, $num_high, true, false);
+        $message = "Is $value between [{$num_low},{$num_high}[";
+        $this->assertTrue($output, $message);
+        
+        $output = Num::between($value, $num_low, $num_high, true, true);
+        $message = "Is $value between [{$num_low},{$num_high}]";
+        $this->assertTrue($output, $message);
+
+        /**
+         * Tests for value = 12
+         */
+        $num_low = "7";
+        $num_high = 12;
         $value = 12;
-        $this->assertEquals(false, Num::between($value, $str, $num_high, false, false), "Is $value between ]{$str},{$num_high}[");
-        $this->assertEquals(true, Num::between($value, $str, $num_high, false, true), "Is $value between ]{$str},{$num_high}]");
-        $this->assertEquals(false, Num::between($value, $str, $num_high, true, false), "Is $value between [{$str},{$num_high}[");
-        $this->assertEquals(true, Num::between($value, $str, $num_high, true, true), "Is $value between [{$str},{$num_high}]");
+        
+        $output = Num::between($value, $num_low, $num_high, false, false);
+        $message = "Is $value between ]{$num_low},{$num_high}[";
+        $this->assertFalse($output, $message);
 
+        $output = Num::between($value, $num_low, $num_high, false, true);
+        $message = "Is $value between ]{$num_low},{$num_high}]";
+        $this->assertTrue($output, $message);
+        
+        $output = Num::between($value, $num_low, $num_high, true, false);
+        $message = "Is $value between [{$num_low},{$num_high}[";
+        $this->assertFalse($output, $message);
+        
+        $output = Num::between($value, $num_low, $num_high, true, true);
+        $message = "Is $value between [{$num_low},{$num_high}]";
+        $this->assertTrue($output, $message);
+
+        /**
+         * Tests for value = 2
+         */
+        $num_low = 12;
+        $num_high = 3;
         $value = 2;
-        $this->assertEquals(false, Num::between($value, $num_high, $num_low, false, false), "Is $value between ]{$num_low},{$num_high}[");
-        $this->assertEquals(false, Num::between($value, $num_high, $num_low, false, true), "Is $value between ]{$num_low},{$num_high}]");
-        $this->assertEquals(false, Num::between($value, $num_high, $num_low, true, false), "Is $value between [{$num_low},{$num_high}[");
-        $this->assertEquals(false, Num::between($value, $num_high, $num_low, true, true), "Is $value between [{$num_low},{$num_high}]");
 
-        $value = 5;
-        $this->assertEquals(true, Num::between($value, $num_high, $num_low, false, false), "Is $value between ]{$num_low},{$num_high}[");
-        $this->assertEquals(true, Num::between($value, $num_high, $num_low, false, true), "Is $value between ]{$num_low},{$num_high}]");
-        $this->assertEquals(true, Num::between($value, $num_high, $num_low, true, false), "Is $value between [{$num_low},{$num_high}[");
-        $this->assertEquals(true, Num::between($value, $num_high, $num_low, true, true), "Is $value between [{$num_low},{$num_high}]");
+        $output = Num::between($value, $num_low, $num_high, false, false);
+        $message = "Is $value between ]{$num_low},{$num_high}[";
+        $this->assertFalse($output, $message);
+
+        $output = Num::between($value, $num_low, $num_high, false, true);
+        $message = "Is $value between ]{$num_low},{$num_high}]";
+        $this->assertFalse($output, $message);
+        
+        $output = Num::between($value, $num_low, $num_high, true, false);
+        $message = "Is $value between [{$num_low},{$num_high}[";
+        $this->assertFalse($output, $message);
+        
+        $output = Num::between($value, $num_low, $num_high, true, true);
+        $message = "Is $value between [{$num_low},{$num_high}]";
+        $this->assertFalse($output, $message);
+
+        /**
+         * Tests for value = "5"
+         */
+        $num_low = 12;
+        $num_high = 3;
+        $value = "5";
+
+        $output = Num::between($value, $num_low, $num_high, false, false);
+        $message = "Is $value between ]{$num_low},{$num_high}[";
+        $this->assertTrue($output, $message);
+
+        $output = Num::between($value, $num_low, $num_high, false, true);
+        $message = "Is $value between ]{$num_low},{$num_high}]";
+        $this->assertTrue($output, $message);
+        
+        $output = Num::between($value, $num_low, $num_high, true, false);
+        $message = "Is $value between [{$num_low},{$num_high}[";
+        $this->assertTrue($output, $message);
+        
+        $output = Num::between($value, $num_low, $num_high, true, true);
+        $message = "Is $value between [{$num_low},{$num_high}]";
+        $this->assertTrue($output, $message);
     }
 }
